0c0c623617569fff9cf48a0953e978f92af99506
santiago.rodriguez <analista.desarrollo1@microminerales.com>
2025-06-03 00:52:20 -0500

    Add contries API Controller

--- Fantasy/Fantasy.Backend/Program.cs
+++ Fantasy/Fantasy.Backend/Program.cs
@@ -1,11 +1,28 @@
-using Fantasy.Backend.Data;
+﻿using Fantasy.Backend.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Fantasy API",
+        Version = "v1",
+        Description = "API para Fantasy App"
+    });
+
+    // ✅ Agrega el servidor base aquí
+    options.AddServer(new OpenApiServer
+    {
+        Url = "https://localhost:7165", // Cambia esto según el puerto de tu API
+        Description = "Servidor local de desarrollo"
+    });
+});
+
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConection"));
 var app = builder.Build();
 
